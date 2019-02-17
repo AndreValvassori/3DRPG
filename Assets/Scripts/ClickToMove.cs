aa51ebd3ec1557212349f1ceaf0de6fce4fb654c
André Victor Stephanes Valvassori <an.victor@hotmail.com>
2019-02-17 12:14:43 -0300

    First Commit!

---
+++ Assets/Scripts/ClickToMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class ClickToMove : MonoBehaviour
+{
+    private NavMeshAgent mNavMeshAgent;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        mNavMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if(Physics.Raycast(ray, out hit, 100))
+            {
+                mNavMeshAgent.destination = hit.point;
+            }
+        }
+    }
+}
