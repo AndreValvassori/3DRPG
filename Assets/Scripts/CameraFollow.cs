e96588c1f48b84eb8369c72424d48fd1979d3a44
André Victor Stephanes Valvassori <an.victor@hotmail.com>
2019-02-22 00:14:34 -0300

    Camera zoom function

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -6,14 +6,47 @@ public class CameraFollow : MonoBehaviour
 {
     public Transform target;
     public float SmoothSpeed = 0.125f;
+
     private Vector3 velocity = Vector3.zero;
 
+    public int MinFieldOfView;
+    public int MaxFieldOfView;
+    public float CamZoomMultiplier;
+
     public Vector3 offset;
-    // Update is called once per frame
+
+    private Camera cam;
+
+    private void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
     void LateUpdate()
     {
         Vector3 desiredPosition = target.position + offset;
-        //Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, SmoothSpeed);
         transform.position = Vector3.SmoothDamp(transform.position,desiredPosition,ref velocity,SmoothSpeed);
+
+
+        var d = Input.GetAxis("Mouse ScrollWheel");
+
+        if (d > 0f)
+        {
+            Debug.Log("Scroll Up " +d);
+           
+            if (cam.fieldOfView >= MinFieldOfView)
+            {
+                cam.fieldOfView = cam.fieldOfView-(d* CamZoomMultiplier);
+            }
+        }
+        else if (d < 0f)
+        {
+            Debug.Log(d);
+            if (cam.fieldOfView <= MaxFieldOfView)
+            {
+                cam.fieldOfView = cam.fieldOfView + ((d*-1) * CamZoomMultiplier);
+            }
+        }
+
     }
 }
