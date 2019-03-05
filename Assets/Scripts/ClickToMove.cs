55503bfd7cfe82cfd1c80098cb64a2a92d349e71
André Victor Stephanes Valvassori <an.victor@hotmail.com>
2019-03-04 23:43:10 -0300

    Interactable Objects Primary Script

--- Assets/Scripts/ClickToMove.cs
+++ Assets/Scripts/ClickToMove.cs
@@ -6,6 +6,9 @@ using UnityEngine;
 public class ClickToMove : MonoBehaviour
 {
     private NavMeshAgent mNavMeshAgent;
+    public Interactable focus;
+    Transform target;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,7 +28,16 @@ public class ClickToMove : MonoBehaviour
         {
             if(Physics.Raycast(ray, out hit, 100))
             {
-                mNavMeshAgent.destination = hit.point;
+                Interactable interactable = hit.collider.GetComponent<Interactable>();
+                if(interactable != null) // Hits a Interactable object.. Like a Chest,Item  or a Box;
+                {
+                    SetFocus(interactable);
+                }
+                else
+                {
+                    RemoveFocus();
+                    mNavMeshAgent.destination = hit.point;
+                }
             }
         }
         if (Input.GetMouseButtonDown(1))
@@ -36,5 +48,56 @@ public class ClickToMove : MonoBehaviour
                 mNavMeshAgent.destination = hit.point;
             }
         }
+
+        if (target != null)
+        {
+            mNavMeshAgent.SetDestination(target.position);
+            FaceTarget();
+        }
+    }
+
+    void SetFocus(Interactable newFocus)
+    {
+        if(newFocus != focus)
+        {
+            if (focus != null)
+                focus.OnDefocused();
+            
+            focus = newFocus;
+
+            FollowTarget(newFocus);
+        }
+        newFocus.OnFocused(transform);
+    }
+
+    void RemoveFocus()
+    {
+        if (focus != null)
+            focus.OnDefocused();
+        focus = null;
+        StopFollowingTarget();
+    }
+
+    public void FollowTarget(Interactable newTarget)
+    {
+        mNavMeshAgent.stoppingDistance = newTarget.radius * .8f;
+        mNavMeshAgent.updateRotation = false;
+
+        target = newTarget.transform;
+
+    }
+
+    public void StopFollowingTarget()
+    {
+        mNavMeshAgent.stoppingDistance = 0f;
+        mNavMeshAgent.updateRotation = true;    
+        target = null;
+    }
+
+    void FaceTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0f, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
     }
 }
--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -1,69 +1,46 @@
-﻿using UnityEngine;
-
-/*	
-	This component is for all objects that the player can
-	interact with such as enemies, items etc. It is meant
-	to be used as a base class.
-*/
+﻿
+using UnityEngine;
 
 public class Interactable : MonoBehaviour
 {
 
-    public float radius = 3f;               // How close do we need to be to interact?
-    public Transform interactionTransform;  // The transform from where we interact in case you want to offset it
+    public float radius = 3f;
+    bool isFocus = false;
+    Transform Player;
 
-    bool isFocus = false;   // Is this interactable currently being focused?
-    Transform player;       // Reference to the player transform
+    bool hasInteracted = false;
 
-    bool hasInteracted = false; // Have we already interacted with the object?
-
-    public virtual void Interact()
+    private void Update()
     {
-        // This method is meant to be overwritten
-        //Debug.Log("Interacting with " + transform.name);
-    }
-
-    void Update()
-    {
-        // If we are currently being focused
-        // and we haven't already interacted with the object
         if (isFocus && !hasInteracted)
         {
-            // If we are close enough
-            float distance = Vector3.Distance(player.position, interactionTransform.position);
+            float distance = Vector3.Distance(Player.position, transform.position);
             if (distance <= radius)
             {
-                // Interact with the object
-                Interact();
                 hasInteracted = true;
+                Debug.Log("Interact");
             }
+
         }
     }
 
-    // Called when the object starts being focused
     public void OnFocused(Transform playerTransform)
     {
         isFocus = true;
-        player = playerTransform;
+        Player = playerTransform;
         hasInteracted = false;
     }
 
-    // Called when the object is no longer focused
     public void OnDefocused()
     {
         isFocus = false;
-        player = null;
+        Player = null;
         hasInteracted = false;
     }
-
-    // Draw our radius in the editor
-    void OnDrawGizmosSelected()
+    private void OnDrawGizmosSelected()
     {
-        if (interactionTransform == null)
-            interactionTransform = transform;
-
         Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(interactionTransform.position, radius);
+        Gizmos.DrawWireSphere(transform.position, radius);
     }
 
 }
